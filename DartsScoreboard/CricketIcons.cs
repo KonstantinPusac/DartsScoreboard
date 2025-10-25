9b135d0da7b96bd3689fb6e905febefd575f4c5f
Pavle Mojasevic <pavle.mojasevic@gmail.com>
2025-10-25 19:09:22 +0200

    feature: cricket

--- DartsScoreboard/CricketIcons.cs
+++ DartsScoreboard/CricketIcons.cs
@@ -9,17 +9,17 @@ public static class CricketIcons
 {
     public const string OneMark =
 @"< svg width = ""16"" height = ""16"" viewBox = ""0 0 16 16"" >
-        < line x1 = ""3"" y1 = ""13"" x2 = ""13"" y2 = ""3"" stroke = ""black"" stroke - width = ""2"" />
+        < line x1 = ""3"" y1 = ""13"" x2 = ""13"" y2 = ""3"" stroke = ""currentColor"" stroke - width = ""2"" />
     </ svg >";
     public const string TwoMarks =
 @"< svg width = ""16"" height = ""16"" viewBox = ""0 0 16 16"" >
-        < line x1 = ""3"" y1 = ""13"" x2 = ""13"" y2 = ""3"" stroke = ""black"" stroke - width = ""2"" />
-        < line x1 = ""3"" y1 = ""3"" x2 = ""13"" y2 = ""13"" stroke = ""black"" stroke - width = ""2"" />
+        < line x1 = ""3"" y1 = ""13"" x2 = ""13"" y2 = ""3"" stroke = ""currentColor"" stroke - width = ""2"" />
+        < line x1 = ""3"" y1 = ""3"" x2 = ""13"" y2 = ""13"" stroke = ""currentColor"" stroke - width = ""2"" />
     </ svg >";
     public const string ThreeMarks =
 @"< svg width = ""16"" height = ""16"" viewBox = ""0 0 16 16"" >
-        < line x1 = ""3"" y1 = ""13"" x2 = ""13"" y2 = ""3"" stroke = ""black"" stroke - width = ""2"" />
-        < line x1 = ""3"" y1 = ""3"" x2 = ""13"" y2 = ""13"" stroke = ""black"" stroke - width = ""2"" />
-        < circle cx = ""8"" cy = ""8"" r = ""5"" stroke = ""black"" stroke - width = ""2"" fill = ""none"" />
+        < line x1 = ""3"" y1 = ""13"" x2 = ""13"" y2 = ""3"" stroke = ""currentColor"" stroke - width = ""2"" />
+        < line x1 = ""3"" y1 = ""3"" x2 = ""13"" y2 = ""13"" stroke = ""currentColor"" stroke - width = ""2"" />
+        < circle cx = ""8"" cy = ""8"" r = ""5"" stroke = ""currentColor"" stroke - width = ""2"" fill = ""none"" />
     </ svg >";
 }
